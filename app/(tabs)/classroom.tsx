af6f7a343704c77b79567b0c3ae1b43a9a5571c8
htlost5 <htlost8@gmail.com>
2025-07-13 01:34:54 +0900

    マージブッキングエラー修正後コミット

--- app/(tabs)/classroom.tsx
+++ app/(tabs)/classroom.tsx
@@ -1,5 +1,5 @@
 // app/(tabs)/index.tsx (Home Screen)
-import { View, Text, StyleSheet } from "react-native";
+import { Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Home() {
