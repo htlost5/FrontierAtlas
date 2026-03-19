810d7281bf1df529fd609e1691f54df848ed29e2
htlost5 <htlost8@gmail.com>
2026-03-20 08:22:28 +0900

    script load data from github

---
+++ scripts/build-data-load.js
@@ -0,0 +1,41 @@
+import fs from "fs";
+import path from "path";
+import unzipper from "unzipper";
+import { Buffer } from "buffer";
+
+import { fileURLToPath } from "url";
+
+const versionConfig = JSON.parse(
+  fs.readFileSync(new URL("../config/geo-data-version.json", import.meta.url))
+);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const { version } = versionConfig;
+
+if (!version) {
+  console.error("GetError: version情報を記載してください");
+  process.exit(1);
+}
+
+const BASE_URL = "https://htlost5.github.io/geo-data-repo/releases";
+const ZIP_URL = `${BASE_URL}/${version}/imdf-${version}.zip`;
+
+async function getData() {
+  const response = await fetch(ZIP_URL);
+  if (!response.ok) throw new Error(`Failed to fetch ${ZIP_URL}`);
+
+  const buffer = Buffer.from(await response.arrayBuffer());
+
+  // 上書きするパス
+  const extractPath = path.join(__dirname, "../assets");
+
+  // zip回答
+  const directory = await unzipper.Open.buffer(buffer);
+  await directory.extract({ path: extractPath, concurrency: 5 });
+
+  console.log("Assets updated successfully");
+}
+
+getData().catch(console.error);
